6557a1e98690c5ce62a12ea8c589b5b91f1120d0
AlmasSeth <almasseth302@gmail.com>
2024-06-21 11:34:25 +0300

    first commit

--- app.py
+++ app.py
@@ -1,5 +1,8 @@
 from flask import *
 import pymysql
+
+import hash_function
+
 app = Flask(__name__)
 
 # Routes Here
@@ -12,6 +15,25 @@ def signup():
         #Check if form was posted by user
         #create a function
     if request.method == 'POST':
+            
+            #get form data
+            username = request.form['username']
+            email = request.form['email']
+            password = request.form['password']
+            title = request.form['title']
+
+            connection =pymysql.connect(host='localhost',user='root',password='',database='cyberdb')
+
+            cursor = connection.cursor()
+
+            sql ='''
+                insert into users(username,email,password,title)values(%s,%s,%s,%s)
+                '''
+            
+            cursor.execute(sql,(username,email,password,title))
+
+            connection.commit()
+            
             return render_template('signup.html', msg='Application Made Successfully')
     else:
         # Form not posted, display the form to allow user Post something
